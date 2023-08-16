663791977691ab6088c71bdb18e5ddee991cbf6d
joshuacfowler <jcf3@rice.edu>
2023-08-16 12:48:34 -0400

    starting data processing script to clean archbold demographic data

---
+++ Analyses/data_processing.R
@@ -0,0 +1,21 @@
+####################################################################################
+# Purpose: Read Archbold demographic data from eleven species and process them to have consistent format for vital rate analyses
+# Author: Joshua Fowler
+# Date: Aug 16, 2023
+####################################################################################
+
+##### Set up #####
+
+library(renv) # track package versions
+# renv::snapshot()
+# renv::restore()
+
+
+library(tidyverse)
+
+
+
+
+####################################################################################
+
+####################################################################################
