56e0d03d5ece7eb781b9c61c3c992d967a73f831
joshuacfowler <jcf3@rice.edu>
2024-09-10 15:47:34 -0400

    starting reproductive vital rate models

--- Analyses/vital_rate_analysis.R
+++ Analyses/vital_rate_analysis.R
@@ -7,9 +7,10 @@
 
 library(renv) # track package versions
 # renv::init()
-# renv::snapshot()
+renv::snapshot()
+# ren
 renv::restore()
-
+# renv::update()
 
 library(tidyverse)
 library(readxl)
@@ -60,25 +61,48 @@ NPkurtosis=function(x,p=0.05) {
 ###### sourcing in the cleaned vital rate data #####################################
 ####################################################################################
 # source("data.processing.R")
-ERYCUN <- ERYCUN_covariates
-
+ERYCUN <- ERYCUN_covariates %>% 
+  mutate(log_ros_diameter.t = log(ros_diameter.t)) %>% 
+  filter(Site_tag != "royce_ranch" & bald != "200")%>% 
+  filter(year.t >1989)
+  
+PARCHA <- PARCHA_covariates
 
 ####################################################################################
 ###### filtering out NAs for each vital rate #####################################
 ####################################################################################
+# ERYCUN
 
 ERYCUN_surv.df <- ERYCUN %>% 
-  filter(!is.na(surv.t1) & !is.na(ros_diameter.t)) %>% 
-  mutate(log_ros_diameter.t = log(ros_diameter.t)) %>% 
-  filter(Site_tag != "royce_ranch" & bald != "200")%>% 
-  filter(year.t >1989)
-
-
+  filter(!is.na(surv.t1) & !is.na(log_ros_diameter.t)) 
+ERYCUN_flw_status.df <- ERYCUN %>% 
+  filter(!is.na(flw_status.t) & !is.na(log_ros_diameter.t)) 
+ERYCUN_flw_stem.df <- ERYCUN %>% 
+  filter(flw_status.t == 1 & !is.na(flw_stem.t) & !is.na(log_ros_diameter.t))
+ERYCUN_flw_head.df <- ERYCUN %>% 
+  filter(flw_status.t == 1 & !is.na(flw_head.t) & !is.na(log_ros_diameter.t))
 ERYCUN_growth.df <- ERYCUN %>% 
-  filter(!is.na(ros_diameter.t1) & !is.na(ros_diameter.t)) %>% 
-  mutate(log_ros_diameter.t = log(ros_diameter.t)) %>% 
-  filter(Site_tag != "royce_ranch" & bald != "200")%>% 
-  filter(year.t >1989)
+  filter(!is.na(ros_diameter.t1) & !is.na(log_ros_diameter.t)) 
+
+# we can filter out some of the sites, but need to get more of the meta-data about that
+# PARCHA
+PARCHA_surv.df <- PARCHA %>% 
+  filter(!is.na(surv.t1)) %>% 
+  group_by(plant_id) %>%  
+  mutate(census_end = case_when(surv.t1 == 0 ~ census_date.t1),
+         census_start = min(census_date.t1)) %>% 
+  fill(census_end, .direction = "updown") %>% 
+  fill(census_start, .direction = "updown") %>% 
+  ungroup() %>% 
+  mutate(age =month(age.t))
+  
+
+
+PARCHA_flowering.df <- PARCHA %>% 
+  filter(month.t1 %in% c(8,11)) %>% 
+  mutate(flw.t1 = case_when(birth_date == census_date.t1 & surv.t1 == 1 & is.na(flw.t1) ~ "0",
+                            TRUE ~ flw.t1)) %>% 
+  filter(!is.na(flw.t1))  
 
 
 
@@ -94,7 +118,7 @@ set.seed(123)
 ## MCMC settings
 mcmc_pars <- list(
   warmup = 1000, 
-  iter = 2500, 
+  iter = 2000, 
   thin = 1, 
   chains = 3
 )
@@ -102,9 +126,9 @@ mcmc_pars <- list(
 
 
 ####################################################################################
-###### Survival models #############################################################
+# Survival models ------------------------------------------------------------------
 ####################################################################################
-
+### ERYCUN  ----
 # starting first with a model without environmental covariates
 erycun.survival <- brm(surv.t1~ 1 + log_ros_diameter.t + I(log_ros_diameter.t^2) + fire_frequency_actual + (1|bald), data = ERYCUN_surv.df,
               family = "bernoulli",
@@ -147,6 +171,98 @@ ggplot(data = prediction_df)+
   geom_line(aes(x = fire_frequency_actual, y = fit, color = as.factor(log_ros_diameter.t), group = log_ros_diameter.t), linewidth = 1) +
   # geom_linerange(aes(x = live_sterile, ymin = lwr, ymax = upr))+ 
   labs(y = "Survival (%)") + theme_minimal()
+
+
+
+
+
+
+### PARCHA  ----
+# starting first with a model without environmental covariates
+# creating a model that models the missing ages data
+
+
+parcha.survival <- brm(surv.t1~ 1 + age, data = PARCHA_surv.df,
+                       family = "bernoulli",
+                       prior = c(set_prior("normal(0,1)", class = "b")),
+                       warmup = mcmc_pars$warmup, iter = mcmc_pars$iter, chains = mcmc_pars$chains)
+
+
+prediction_df <- expand.grid(age = seq(from = min(PARCHA_surv.df$age, na.rm = T), to = max(PARCHA_surv.df$age, na.rm  = T)),
+                             #fire_frequency_actual = seq(from = min(ERYCUN_surv.df$fire_frequency_actual, na.rm = T), to = max(ERYCUN_surv.df$fire_frequency_actual, na.rm = T), length.out = 25),
+                             bald = NA)
+
+preds <- fitted(parcha.survival, newdata = prediction_df)
+prediction_df$fit <- preds[,"Estimate"]
+prediction_df$lwr <- preds[,"Q2.5"]
+prediction_df$upr <- preds[,"Q97.5"]
+
+
+# Making a binned survival df for visualization
+parcha.survival.binned <- PARCHA_surv.df %>% 
+  ungroup() %>% 
+  # mutate(age_bin = cut_number(age, 10)) %>% 
+  group_by(age) %>% 
+  summarize(mean_age = mean(age, na.rm = T),
+            mean_surv = mean(surv.t1, na.rm = T),
+            samplesize = n())
+
+
+ggplot(data = prediction_df)+
+  geom_point(data = PARCHA_surv.df, aes( x= age, y = surv.t1), color = "grey55", alpha = .2)+
+  geom_point(data = parcha.survival.binned, aes( x= mean_age, y = mean_surv, size = samplesize), color = "skyblue3", alpha = .5)+
+  geom_ribbon(aes(x = age, ymax = upr, ymin = lwr), alpha = .4)+
+  geom_line(aes(x = age, y = fit), linewidth = 1) +
+  lims(y = c(0,1))+
+  labs(y = "Survival (%)") + theme_minimal()
+
+
+
+
+formula.surv <- bf(surv.t1~ 1 + mi(age))+ bernoulli()
+formula.age <- bf(age|mi() ~ 1 + census_start + census_end) + gaussian()
+parcha.survival <- brm(formula.surv + formula.age + set_rescor(FALSE), data = PARCHA_surv.df,
+                       prior = c(set_prior("normal(0,1)", class = "b")),
+                       warmup = mcmc_pars$warmup, iter = mcmc_pars$iter, chains = mcmc_pars$chains)
+
+prediction_df <- PARCHA_surv.df %>% 
+  mutate(census_start = as.numeric(census_start))
+pp_check(parcha.survival, resp = "survt1")
+conditional_effects(parcha.survival, resp = "survt1")
+
+preds <- fitted(parcha.survival, newdata = prediction_df)
+prediction_df$fit.surv <- preds[,"Estimate","survt1"]
+prediction_df$lwr.surv <- preds[,"Q2.5","survt1"]
+prediction_df$upr.surv <- preds[,"Q97.5","survt1"]
+
+prediction_df$fit.age <- preds[,"Estimate","age"]
+prediction_df$lwr.age <- preds[,"Q2.5","age"]
+prediction_df$upr.age <- preds[,"Q97.5","age"]
+
+
+ggplot(data = prediction_df)+
+  geom_point(data = PARCHA_surv.df, aes(x = age, y = surv.t1), color = "grey55", alpha = .2)+
+  geom_point(data = parcha.survival.binned, aes( x= mean_age, y = mean_surv, size = samplesize), color = "skyblue3", alpha = .5)+
+  geom_ribbon(aes(x = age, ymax = upr.surv, ymin = lwr.surv), alpha = .4)+
+  geom_line(aes(x = age, y = fit.surv), linewidth = 1) +
+  lims(y = c(0,1))+
+  labs(y = "Survival (%)") + theme_minimal()
+
+
+ggplot(data = prediction_df)+
+  # geom_histogram(data = PARCHA_surv.df, aes(x = age), color = "grey55", alpha = .2)+
+  # geom_linerange(aes(x = age, ymax = upr.age, ymin = lwr.age), alpha = .4)+
+  geom_point(aes(x = age, y = fit.age), linewidth = 1) +
+  # lims(y = c(0,1))+
+  labs(y = "predicted age") + theme_minimal()
+
+
+
+saveRDS(parcha.survival, "parcha.survival.rds")
+
+
+
+
 
 
 ####################################################################################
@@ -227,6 +343,103 @@ erycun.growth <- stan(file = "Analyses/growth_model.stan",
 
 
 print(erycun.growth)
+
+
+
+
+####################################################################################
+###### flowering models ###############################################################
+####################################################################################
+### ERYCUN  ----
+# starting first with a model without environmental covariates
+erycun.flw_status <- brm(flw_status.t~ 1 + s(log_ros_diameter.t, bs = "tp") + fire_frequency_actual + (1|bald), data = ERYCUN_flw_status.df,
+                       family = "bernoulli",
+                       prior = c(set_prior("normal(0,1)", class = "b")),
+                       warmup = mcmc_pars$warmup, iter = mcmc_pars$iter, chains = mcmc_pars$chains)
+
+saveRDS(erycun.flw_status, "erycun.flw_status.rds")
+
+
+erycun.flw_stem <- brm(flw_stem.t~ 1 + s(log_ros_diameter.t, bs = "tp") + fire_frequency_actual + (1|bald), data = ERYCUN_flw_stem.df,
+                         family = "negbinomial",
+                         prior = c(set_prior("normal(0,1)", class = "b")),
+                         warmup = mcmc_pars$warmup, iter = mcmc_pars$iter, chains = mcmc_pars$chains)
+# Making prediction dataframe
+
+prediction_df <- expand.grid(log_ros_diameter.t = seq(from = min(ERYCUN_flw_status.df$log_ros_diameter.t, na.rm = T), to = max(ERYCUN_flw_status.df$log_ros_diameter.t, na.rm  = T), length.out = 25),
+                             fire_frequency_actual = seq(from = min(ERYCUN_flw_status.df$fire_frequency_actual, na.rm = T), to = max(ERYCUN_flw_status.df$fire_frequency_actual, na.rm = T), length.out = 10),
+                             bald = NA)
+
+preds.flw_status <- fitted(erycun.flw_status, newdata = prediction_df)
+prediction_df$fit <- preds.flw_status[,"Estimate"]
+prediction_df$lwr <- preds.flw_status[,"Q2.5"]
+prediction_df$upr <- preds.flw_status[,"Q97.5"]
+
+
+# Making a binned survival df for visualization
+erycun.flw_status.binned <- ERYCUN_flw_status.df %>% 
+  ungroup() %>% 
+  mutate(size_bin = cut_number(log_ros_diameter.t, 10)) %>% 
+  group_by(size_bin) %>% 
+  summarize(mean_size = mean(log_ros_diameter.t, na.rm = T),
+            mean_flw_status = mean(flw_status.t, na.rm = T),
+            samplesize = n())
+
+
+
+preds.flw_stems <- fitted(erycun.flw_stem, newdata = prediction_df)
+prediction_df$fit <- preds.flw_stems[,"Estimate"]
+prediction_df$lwr <- preds.flw_stems[,"Q2.5"]
+prediction_df$upr <- preds.flw_stems[,"Q97.5"]
+
+
+# Making a binned survival df for visualization
+erycun.flw_stems.binned <- ERYCUN_flw_stem.df %>% 
+  ungroup() %>% 
+  mutate(size_bin = cut_number(log_ros_diameter.t, 10)) %>% 
+  group_by(size_bin) %>% 
+  summarize(mean_size = mean(log_ros_diameter.t, na.rm = T),
+            mean_flw_stems = mean(flw_stem.t, na.rm = T),
+            samplesize = n())
+
+
+
+
+# now we can plot the model predictions
+
+# ggplot(data = prediction_df)+
+#   geom_point(data = ERYCUN_flw_status.df, aes( x= fire_frequency_actual, y = flw_status.t), color = "grey55", alpha = .2)+
+#   # geom_point(data = erycun.flw_status.binned, aes( x= mean_size, y = mean_surv, size = samplesize), color = "skyblue3", alpha = .5)+
+#   geom_ribbon(aes(x = fire_frequency_actual, ymax = upr, ymin = lwr,fill = as.factor(log_ros_diameter.t),  group = log_ros_diameter.t), alpha = .4)+
+#   geom_line(aes(x = fire_frequency_actual, y = fit, color = as.factor(log_ros_diameter.t), group = log_ros_diameter.t), linewidth = 1) +
+#   # geom_linerange(aes(x = live_sterile, ymin = lwr, ymax = upr))+ 
+#   labs(y = "Flowering (%)") + theme_minimal()
+
+
+
+ggplot(data = prediction_df)+
+  # geom_point(data = ERYCUN_flw_status.df, aes( x= fire_frequency_actual, y = flw_status.t), color = "grey55", alpha = .2)+
+  geom_point(data = erycun.flw_status.binned, aes( x= mean_size, y = mean_flw_status, size = samplesize), color = "skyblue3", alpha = .9)+
+  # geom_ribbon(aes(x = fire_frequency_actual, ymax = upr, ymin = lwr,fill = as.factor(log_ros_diameter.t),  group = log_ros_diameter.t), alpha = .4)+
+  geom_ribbon(aes(x = log_ros_diameter.t, ymin = lwr, ymax = upr, fill = fire_frequency_actual, group = fire_frequency_actual), alpha = .05) +
+  geom_line(aes(x = log_ros_diameter.t, y = fit, color = fire_frequency_actual, group = fire_frequency_actual), alpha = .6) +
+  # geom_linerange(aes(x = live_sterile, ymin = lwr, ymax = upr))+ 
+  labs(y = "Flowering (%)") + theme_minimal()
+
+
+ggplot(data = prediction_df)+
+  geom_point(data = ERYCUN_flw_stem.df, aes( x= log_ros_diameter.t, y = flw_stem.t), color = "grey55", alpha = .2)+
+  geom_point(data = erycun.flw_stems.binned, aes( x= mean_size, y = mean_flw_stems, size = samplesize), color = "skyblue3", alpha = .9)+
+  # geom_ribbon(aes(x = fire_frequency_actual, ymax = upr, ymin = lwr,fill = as.factor(log_ros_diameter.t),  group = log_ros_diameter.t), alpha = .4)+
+  geom_ribbon(aes(x = log_ros_diameter.t, ymin = lwr, ymax = upr, fill = fire_frequency_actual, group = fire_frequency_actual), alpha = .05) +
+  geom_line(aes(x = log_ros_diameter.t, y = fit, color = fire_frequency_actual, group = fire_frequency_actual), alpha = .6) +
+  # geom_linerange(aes(x = live_sterile, ymin = lwr, ymax = upr))+ 
+  labs(y = "Flowering (%)") + theme_minimal()
+
+
+
+
+
 ####################################################################################
 ###### Posterior predictive checks #################################################
 ####################################################################################
