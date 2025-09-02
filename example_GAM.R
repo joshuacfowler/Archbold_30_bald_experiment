72c655dd9bab0cce5f6f87f328fbf1c8bb6ebfd8
joshuacfowler <jcf3@rice.edu>
2025-09-02 17:00:49 -0600

    finally figured out how to pull out the basis matrix from the brms gam. I slight difference is predicting with mean of posteriors, rather than calculating after the fact mean of all posteriors.

--- example_GAM.R
+++ example_GAM.R
@@ -1,4 +1,5 @@
 library(brms)
+library(tidyverse)
 set.seed(123)
 
 # Generate some data
@@ -34,8 +35,8 @@ smooth_term_contribution <- linpred_all - fixed_effects
 # You can now calculate the mean or credible intervals for the smooth term
 
 smooth_mean <- apply(linpred_all, 2, mean)
-smooth_lower_ci <- apply(smooth_term_contribution, 2, quantile, probs = 0.025)
-smooth_upper_ci <- apply(smooth_term_contribution, 2, quantile, probs = 0.975)
+smooth_lower_ci <- apply(linpred_all, 2, quantile, probs = 0.025)
+smooth_upper_ci <- apply(linpred_all, 2, quantile, probs = 0.975)
 
 # You can now plot these results
 
@@ -70,7 +71,7 @@ fit_gam_mgcv <- gam(y ~ s(x), data = dat)
 
 # Create new data
 
-new_dat <- data.frame(x = c(1,10))#seq(0, 10, length.out = 50))
+new_dat <- data.frame(x = seq(0, 10, length.out = 50))
 # Step 3: Get the design matrix for new data
 
 #Use the predict.gam function with the type = "lpmatrix" argument. This will return the linear predictor design matrix, which includes both the intercept and the smooth term basis functions.
@@ -97,6 +98,30 @@ smooth_design_matrix <- X_new[, -1]
 # You can check the column names to confirm
 colnames(smooth_design_matrix)
 
+
+
+# Another way to generate these similar kinds of matrices which are what is done before fitting model in mgcv
+sm <- smoothCon(
+  s(x),
+  data=new_dat,
+  absorb.cons = T,
+  diagonal.penalty = T
+)
+
+# function that restructures basis in terms of random effects formulation of splines
+re <- smooth2random(sm[[1]], "", type=2)
+
+X_new <- re$Xf
+Z_new <- re$rand$Xr
+
+
+brms:::data_sm(brms:::model.frame.brmsfit(fit_gam), newdata = new_dat)
+
+stan_data <- make_standata(fit_gam)
+
+
+
+
 # Note: In a brms context, the predict function is handled internally. brms extracts the necessary information from the mgcv object it creates during the model fitting process to perform this calculation for you automatically when you use posterior_linpred() or posterior_predict() with new data.
 
 # refitting the brms model cause simulated data is slightly different
@@ -110,9 +135,81 @@ fit_gam <- brm(
 
 # then extracting the intercept and smooth parameter posteriors which will get multiplied by the smooth design matrix in the linear predictor
 post_draws <- as_draws_df(fit_gam)
-post_int <- post_draws$b_Intercept[1]
 
-post_smooth_intercept <- post_draws$bs_sx_1[1]
-post_smooth_matrix <- as.matrix( post_draws[,grep("s_sx_1\\[", names(post_draws), value = TRUE)])
+# coef <- c(post_draws$b_Intercept[1],
+#           post_draws$bs_sx_1[1],
+#           post_draws)
+post_int <- mean(post_draws$b_Intercept)
+post_bX <- mean(post_draws$bs_sx_1)
+post_smooth <- unlist(colMeans(post_draws[,6:13])) # "s_sx_1[1]":"s_sx_8[1]"
 
-lin_pred <- post_int + post_smooth_intercept*new_dat + smooth_design_matrix%*% t(post_smooth_matrix[1,])
+# post_smooth_matrix <- matrix(data = NA, nrow = 1, ncol = 9)
+# post_smooth_matrix[,1] <- post_draws$bs_sx_1[1]
+# post_smooth_matrix[,2:9] <- as.matrix(post_draws[,grep("s_sx_1\\[", names(post_draws), value = TRUE)][1,])
+# post_smooth_intercept <- post_draws$bs_sx_1[1]
+# post_smooth_matrix <- as.matrix( post_draws[,grep("s_sx_1\\[", names(post_draws), value = TRUE)])[1,]
+prep <- prepare_predictions(fit_gam, newdata = new_dat)
+
+
+pred <- tibble(lin_pred = post_int + post_bX*X_new+ Z_new %*% (post_smooth), # 
+               lin_pred_prep = mean(prep$dpars$mu$fe$b) + prep$dpars$mu$sm$fe$Xs*mean(prep$dpars$mu$sm$fe$bs) + prep$dpars$mu$sm$re$sx$Zs[[1]] %*% colMeans(prep$dpars$mu$sm$re$sx$s[[1]]),
+               lin_rnorm = rnorm(lin_pred_prep, sd = mean(prep$dpars$sigma)),
+               x = new_dat$x,
+               default = predict(fit_gam, new_dat)[,"Estimate"],
+               just_smooth = c(colMeans(brms:::posterior_smooths(fit_gam, newdata = new_dat, smooth = "s(x)"))),
+               smooth_plus = post_int + c(colMeans(brms:::posterior_smooths(fit_gam, newdata = new_dat, smooth = "s(x)"))))
+                 
+          
+
+# plot( pred$x, pred$lin_pred)
+
+
+ggplot(dat)+
+  geom_point(aes(x = x, y = y))+
+  geom_line(data = pred, aes(x = x, y = lin_pred) )+
+  geom_line(data = pred, aes(x = x, y = default), color = "red")+
+  geom_line(data = pred, aes(x = x, y = lin_pred_prep), color = "orange")+
+  geom_line(data = pred, aes(x = x, y = lin_pred_prep), color = "blue")
+  geom_line(data = pred, aes(x = x, y = just_smooth), color = "blue")+
+  geom_line(data = pred, aes(x = x, y = smooth_plus), color = "green")
+
+
+
+
+
+Xp <- predict(fit_gam_mgcv,new_dat,type="lpmatrix") 
+
+## Xp %*% coef(b) yields vector of predictions
+pred <- Xp %*% coef(fit_gam_mgcv)
+
+
+
+
+
+
+Xp <-  predict(fit_gam_mgcv, newdata = new_dat, type = "lpmatrix")
+
+xn <- c(.341,.122,.476,.981) ## want prediction at these values
+x0 <- 1         ## intercept column
+dx <- 1/30      ## covariate spacing in `newd'
+for (j in 0:2) { ## loop through smooth terms
+  cols <- 1+j*9 +1:9      ## relevant cols of Xp
+  i <- floor(xn[j+1]*30)  ## find relevant rows of Xp
+  w1 <- (xn[j+1]-i*dx)/dx ## interpolation weights
+  ## find approx. predict matrix row portion, by interpolation
+  x0 <- c(x0,Xp[i+2,cols]*w1 + Xp[i+1,cols]*(1-w1))
+}
+dim(x0)<-c(1,28) 
+fv <- x0%*%coef(b) + xn[4];fv    ## evaluate and add offset
+se <- sqrt(x0%*%b$Vp%*%t(x0));se ## get standard error
+## compare to normal prediction
+predict(b,newdata=data.frame(x0=xn[1],x1=xn[2],
+                             x2=xn[3],x3=xn[4]),se=TRUE)
+
+
+
+
+
+
+
+
